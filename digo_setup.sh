55b9ab72f8bc5ba7041294c63caa4501b37e9684
onkel dunkel <rasbalnor@gmail.com>
2019-05-29 08:45:12 +0200

    fixed setup file

--- digo_setup.sh
+++ digo_setup.sh
@@ -1,17 +1,19 @@
 
 
-#update link***********************************************************************
-wget -O books.zip http://www.mediafire.com/file/2a6b30tg30e901m/books_txts.zip/file
-apt install -y unzip
-unzip books.zip
+apt update
 
 apt install -y python3-pip
 pip3 install spacy
 python3 -m spacy download en_core_web_sm
 pip3 install json
 pip3 install mysql-connector-python
+apt install -y unzip
+
+#update link***********************************************************************
+wget -O books.zip http://www.mediafire.com/file/2a6b30tg30e901m/books_txts.zip/file
+unzip books.zip
+
 
 #update link*********************************
-wget -O process_books.py http://www.mediafire.com/file/ceqpp37gijcy3ad/process_cities.py/file
-
-python3 process_books.py
+#wget -O process_books.py http://www.mediafire.com/file/qynfcw0cm2ceyjx/process_books.py/file
+#python3 process_books.py
